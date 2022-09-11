2876667077a2c023d526c3779515a421d29d8ee5
Raks93 <37742982+Raks93@users.noreply.github.com>
2022-09-11 20:53:29 +0500

    complete second task

---
+++ src/homework/task2/Main.java
@@ -0,0 +1,33 @@
+package homework.task2;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findSumTwoNumbersEqualsNumber(new Integer[]{3, 4, 2, 7}, 10)));
+
+    }
+
+    public static Integer[] findSumTwoNumbersEqualsNumber (Integer[] array, Integer goal) {
+
+        if (Objects.isNull(goal)) {
+            return new Integer[]{0, 0};
+        }
+        Integer[] filterArray = Arrays.stream(array)
+                .filter(Objects::nonNull)
+                .toArray(Integer[]::new);
+
+        for (int i = 0; i < filterArray.length - 1; i++) {
+                for (int j = i; j < filterArray.length; j++) {
+                    if (filterArray[i] + filterArray[j] == goal) {
+                        return new Integer[] {filterArray[i], filterArray[j]};
+                    }
+                }
+            }
+
+
+        return new Integer[]{0, 0};
+    }
+}
